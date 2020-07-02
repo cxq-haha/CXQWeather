c0e2fe15446057f1115730b47a75dd55b7b7b5f6
cxq <1979553877@qq.com>
2020-07-02 13:31:32 +0800

    加入创建数据库和g表的各项配置

---
+++ app/src/main/java/com/cxqweather/android/db/City.java
@@ -0,0 +1,62 @@
+package com.cxqweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int ProvinceId;
+
+    public City() {
+    }
+
+    public City(int id, String cityName, int cityCode, int provinceId) {
+        this.id = id;
+        this.cityName = cityName;
+        this.cityCode = cityCode;
+        ProvinceId = provinceId;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", cityName='" + cityName + '\'' +
+                ", cityCode=" + cityCode +
+                ", ProvinceId=" + ProvinceId +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return ProvinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        ProvinceId = provinceId;
+    }
+}
