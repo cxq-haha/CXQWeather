a21f04b0fb11c93ee3ab6d2f41b731d967a3745f
cxq <1979553877@qq.com>
2020-07-02 23:12:42 +0800

    写完天气界面

---
+++ app/src/main/java/com/cxqweather/android/WeatherActivity.java
@@ -0,0 +1,15 @@
+package com.cxqweather.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+    }
+}
--- app/src/main/java/com/cxqweather/android/util/Utility.java
+++ app/src/main/java/com/cxqweather/android/util/Utility.java
@@ -5,6 +5,10 @@ import android.text.TextUtils;
 import com.cxqweather.android.db.City;
 import com.cxqweather.android.db.County;
 import com.cxqweather.android.db.Province;
+import com.cxqweather.android.gson.Weather;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParseException;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -86,5 +90,22 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     * @param response
+     * @return
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
